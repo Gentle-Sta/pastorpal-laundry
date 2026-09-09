957926c417c45d8c425c9294beadbbeb56aa6fdb
GentledevX <princeorji901@gmail.com>
2026-09-09 12:29:28 +0100

    Update Supabase URL in supabase.js

--- frontend/supabase.js
+++ frontend/supabase.js
@@ -1,5 +1,5 @@
 // supabase.js
-const SUPABASE_URL = "https://lbynmptomywbkpukjxex.supabase.co";
+const SUPABASE_URL = "https://rpcczulnkogylqjzlikq.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxieW5tcHRvbXl3YmtwdWtqeGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MDg2ODUsImV4cCI6MjA3NTk4NDY4NX0.-O7JU8hjLYZSwTW7e9vfZz6n1klpf2pEPWYBgj3i5ZI";
 
 // create supabase client
