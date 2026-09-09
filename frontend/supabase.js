9718bcff5bdfaaeec40e6172d9ee45e2cdd04f1a
GentledevX <princeorji901@gmail.com>
2026-09-09 12:47:03 +0100

    Update Supabase key in supabase.js

--- frontend/supabase.js
+++ frontend/supabase.js
@@ -1,6 +1,6 @@
 // supabase.js
 const SUPABASE_URL = "https://rpcczulnkogylqjzlikq.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxieW5tcHRvbXl3YmtwdWtqeGV4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MDg2ODUsImV4cCI6MjA3NTk4NDY4NX0.-O7JU8hjLYZSwTW7e9vfZz6n1klpf2pEPWYBgj3i5ZI";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJwY2N6dWxua29neWxxanpsaWtxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0MzM2NTUsImV4cCI6MjA3NjAwOTY1NX0.jTbunk_ErVYXEXriO6hWUhcV0MQrlnhkVRoJQHduLRA";
 
 // create supabase client
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
